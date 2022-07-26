7ce45b65b70af7853a5ee7a143a27293c5249979
unknown <you@example.com>
2022-07-26 10:34:11 +0800

    插槽

---
+++ my-blog/MessageTest.vue
@@ -0,0 +1,20 @@
+<template>
+<Message>
+  <div>
+    <a href="">asdfasdf</a>
+    <p>asdfasda</p>
+  </div>
+</Message>
+</template>
+
+<script>
+import Message from "./Message.vue";
+export default {
+  components: {
+    Message,
+  },
+};
+</script>
+
+<style>
+</style>
--- my-blog/src/App.vue
+++ my-blog/src/App.vue
@@ -1,5 +1,32 @@
 <template>
-    <div>
-        <h1>App组件</h1>
-    </div>
+  <div class="app-container">
+    <Layoyt>
+      <template #left>
+        <div class="aside">
+          <SiteAside />
+        </div>
+      </template>
+      <template #default> 中间主区域 </template>
+    </Layoyt>
+  </div>
 </template>
+<script>
+import Layoyt from "./components/Layout";
+import SiteAside from "./components/SiteAside";
+export default {
+  components: {
+    Layoyt,
+    SiteAside,
+  },
+};
+</script>
+<style scoped lang="less">
+@import "~@/styles/mixin.less";
+.app-container {
+  .self-fill(fixed);
+}
+.aside {
+  width: 250px;
+  height: 100%;
+}
+</style>
