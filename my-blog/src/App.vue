c1670e6b14db2ae2b0fd7dd9931df73e886950da
unknown <you@example.com>
2022-07-26 08:51:25 +0800

    组件练习

--- my-blog/src/App.vue
+++ my-blog/src/App.vue
@@ -1,32 +1,5 @@
 <template>
-  <div>
-    <h1>App组件</h1>
-    <Pager
-      :total="total"
-      :current="current"
-      @pageChange="handlePageChange($event)"
-    />
-  </div>
+    <div>
+        <h1>App组件</h1>
+    </div>
 </template>
-
-<script>
-import Pager from "./components/Pager.vue";
-export default {
-  components: {
-    Pager,
-  },
-  data() {
-    return {
-      current: 1,
-      total: 302,
-    };
-  },
-  methods: {
-    handlePageChange(newPage) {
-      this.current = newPage;
-    },
-  },
-};
-</script>
-<style scoped>
-</style>
