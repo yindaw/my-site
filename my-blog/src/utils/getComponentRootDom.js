69aa08c32232f1a7d4d56490a908e83a28518b24
unknown <you@example.com>
2022-07-28 16:12:13 +0800

    弹出消息

---
+++ my-blog/src/utils/getComponentRootDom.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+
+
+/**
+ * 获取某个组件渲染的Dom根元素
+ * @param {*} comp 
+ * @param {*} props 
+ * @returns 
+ */
+export default function getComponentRootDom(comp, props) {
+    const vm = new Vue({
+        render: (h) => h(comp, { props }),
+    });
+    vm.$mount();
+    return vm.$el;
+}
--- my-blog/src/views/Home/index.vue
+++ my-blog/src/views/Home/index.vue
@@ -1,13 +1,34 @@
 <template>
-  <h1>首页</h1>
+  <div class="home-container" ref="container">
+    <h1>首页</h1>
+    <button @click="handleClick">Click</button>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  methods: {
+    handleClick() {
+      // this.$sayHello();
+      this.$showMessage({
+        content: "评论成功",
+        type: "success",
+        container: this.$refs.container,
+        callback: function () {
+          console.log("完成！！！");
+        },
+      });
+    },
+  },
+};
 </script>
 
-<style>
-
+<style scoped>
+.home-container {
+  background: lightblue;
+  width: 300px;
+  border: 1px solid;
+  margin: 50px auto;
+  height: 500px;
+}
 </style>
