892de4dde11661aac13780174f4a271823c09980
unknown <you@example.com>
2022-07-30 10:42:01 +0800

    完成首页-part1

---
+++ my-blog/src/views/Home/CarouselItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="carousel-item-container">CarouselItem</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="less">
+@import "~@/styles/var.less";
+.carousel-item-container{
+    background: @dark;
+    width: 100%;
+    height: 100%;
+    color: #fff;
+}
+</style>
