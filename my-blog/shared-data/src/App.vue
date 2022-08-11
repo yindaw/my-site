865d58391eb38781d7d212e4758a25a774ffadd5
unknown <you@example.com>
2022-08-11 10:50:49 +0800

    数据共享

---
+++ my-blog/shared-data/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <A />
+    <B />
+  </div>
+</template>
+
+<script>
+import A from "./components/A.vue";
+import B from "./components/B.vue";
+export default {
+  components: {
+    A,
+    B,
+  },
+};
+</script>
+
+<style>
+</style>
