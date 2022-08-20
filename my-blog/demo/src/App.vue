3c29d28ae04063a893d5d4981403d21e386a61d9
unknown <you@example.com>
2022-08-20 22:06:55 +0800

    作用域插槽

--- my-blog/demo/src/App.vue
+++ my-blog/demo/src/App.vue
@@ -1,22 +1,46 @@
 <template>
   <div id="app">
-    <input v-model.number="money" type="number" />
-    <p>
-      {{ money | formatMoney(" ") }}
-    </p>
+    <async-content :contentPromise="fetchProducts()">
+      <template #loading>加载中...</template>
+      <template v-slot:default="{ content }">
+        <ul>
+          <li v-for="item in content" :key="item.id">
+            商品名：{{ item.name }} 库存：{{ item.stock }}
+          </li>
+        </ul>
+      </template>
+      <template v-slot:error="{ error }">
+        <p style="color: red">{{ error.message }}</p>
+      </template>
+    </async-content>
   </div>
 </template>
 
 <script>
-import { formatMoney } from "./filter/moneyFilter";
+import AsyncContent from "./components/AsyncContent.vue";
+// ajax模拟函数
+function getProducts() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Math.random() < 0.5) {
+        resolve([
+          { id: 1, name: "xiaomi", stock: 50 },
+          { id: 2, name: "iphone", stock: 70 },
+          { id: 3, name: "huawei", stock: 60 },
+        ]);
+      } else {
+        reject(new Error("not found"));
+      }
+    }, 1000);
+  });
+}
+
 export default {
-  data() {
-    return {
-      money: 1000,
-    };
-  },
-  filters: {
-    formatMoney,
+  components: { AsyncContent },
+  methods: {
+    async fetchProducts() {
+      return await getProducts();
+    },
   },
 };
 </script>
@@ -24,6 +48,9 @@ export default {
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  text-align: center;
+  width: 500px;
+  margin: 1em auto;
+  border: 1px solid #ccc;
+  padding: 20px;
 }
 </style>
