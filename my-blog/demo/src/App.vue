62f1019580be7d6caf2b68c07c96c8bbd8881427
unknown <you@example.com>
2022-08-21 11:42:17 +0800

    过渡和动画

--- my-blog/demo/src/App.vue
+++ my-blog/demo/src/App.vue
@@ -1,56 +1,34 @@
 <template>
   <div id="app">
-    <async-content :contentPromise="fetchProducts()">
-      <template #loading>加载中...</template>
-      <template v-slot:default="{ content }">
-        <ul>
-          <li v-for="item in content" :key="item.id">
-            商品名：{{ item.name }} 库存：{{ item.stock }}
-          </li>
-        </ul>
-      </template>
-      <template v-slot:error="{ error }">
-        <p style="color: red">{{ error.message }}</p>
-      </template>
-    </async-content>
+    <button @click="titleLevel--">level-</button>
+    <button @click="titleLevel++">level+</button>
+    <transition name="title" appear>
+      <component :is="`h${titleLevel}`">title level{{ titleLevel }}</component>
+    </transition>
   </div>
 </template>
 
 <script>
-import AsyncContent from "./components/AsyncContent.vue";
-// ajax模拟函数
-function getProducts() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (Math.random() < 0.5) {
-        resolve([
-          { id: 1, name: "xiaomi", stock: 50 },
-          { id: 2, name: "iphone", stock: 70 },
-          { id: 3, name: "huawei", stock: 60 },
-        ]);
-      } else {
-        reject(new Error("not found"));
-      }
-    }, 1000);
-  });
-}
-
 export default {
-  components: { AsyncContent },
-  methods: {
-    async fetchProducts() {
-      return await getProducts();
-    },
+  data() {
+    return {
+      titleLevel: 1,
+    };
   },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  width: 500px;
-  margin: 1em auto;
-  border: 1px solid #ccc;
-  padding: 20px;
+  text-align: center;
+}
+.title-enter,
+.title-leave-to {
+  opacity: 0;
+}
+
+.title-enter-active,
+.title-leave-active {
+  transition: 3s;
 }
 </style>
